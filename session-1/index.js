bc6b0d0838541b532d64814efc8347b46340a466
kawtaraitsaid <156421778+kawtaraitsaid@users.noreply.github.com>
2024-01-29 15:09:44 +0100

    test git

---
+++ session-1/index.js
@@ -0,0 +1,40 @@
+//declaration des variable/*
+/*les commentaires multilignes*/
+
+/*let nombreUn;
+let nombreDeux;
+let somme;
+
+const PI = 3.14;
+nombreUn = 16;
+nombreDeux = 20;
+
+somme = nombreUn + nombreDeux;
+console.log(somme)
+
+//procedure
+  function greet(nom) {
+   console.log("helloo!"+ nom)
+}
+
+greet("bakarouch")
+greet("boukri")
+greet("badioui")*/
+//function
+ /* function somme(x,y) {
+    
+    return x+y
+  }
+  console.log(somme(12,30))
+  {
+    function inscrire() {
+      let nom="ait said"
+      let prenom="kawtar"
+      let age="18"
+
+      if (age==18) {
+        
+      }
+    }
+  }*/
+  
---
+++ session-2/index2.js
@@ -0,0 +1,10 @@
+a=10;
+b='10';
+if(a===b){
+   console.log("true") 
+}else if (a!=b){
+    console.log("false")
+};
+
+let statut="etudiant";
+let age=20
